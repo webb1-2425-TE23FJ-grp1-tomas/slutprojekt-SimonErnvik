762e4b4bf1da82462dc5fc7ecb14bd4b6eb4d6ee
simern861 <simern861@edu.linkoping.se>
2025-04-19 11:23:00 +0000

    19/04

--- dellas.js
+++ dellas.js
@@ -1,1 +1,9 @@
 console.log("hej");
+const linkKnapp = document.querySelector(".linkKnapp");
+const linkItem = document.querySelector(".linkItem");
+const linkItem2 = document.querySelector(".linkItem2")
+linkKnapp.addEventListener("click", function(){
+    console.log("klick!");
+    linkItem.classList.toggle("finns");
+    linkItem2.classList.toggle("finns");
+    });
